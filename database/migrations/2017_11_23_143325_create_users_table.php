66869f32ff515fb78113cfd39585855e859fba91
alexeladas <alexeladas@gmail.com>
2017-11-23 11:37:58 -0500

    Alex

---
+++ database/migrations/2017_11_23_143325_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('user_id')->autoIncrement();
+            $table->string('username')->unique;
+            $table->boolean('isadmin');
+            $table->string('password');
+            $table->string('province');
+            $table->string('city');
+            $table->integer('membership_plan');
+            $table->integer('package');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
